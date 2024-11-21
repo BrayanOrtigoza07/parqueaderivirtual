6934e21f2e9f101f7160d3c43a0d9a626839878c
Brayan Camilo Ortigoza Velandia <ortigozavelandiabrayan@gmail.com>
2024-11-21 03:31:30 -0500

    Integración28

--- app/api/history-salidas/route.ts
+++ app/api/history-salidas/route.ts
@@ -6,12 +6,32 @@ const pool = new Pool({
   ssl: { rejectUnauthorized: false },
 });
 
-export async function GET() {
+export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url);
+  const plate = searchParams.get('plate'); // Obtener la placa de los parámetros de consulta, si existe
+
   try {
-    const query = `SELECT * FROM history_salidas ORDER BY exit_time DESC;`;
+    let query = `SELECT * FROM history_salidas`;
+    const values: string[] = [];
+
+    if (plate) {
+      query += ` WHERE plate = $1`;
+      values.push(plate);
+    }
+
+    query += ` ORDER BY exit_time DESC;`;
+
     const client = await pool.connect();
-    const result = await client.query(query);
+    const result = await client.query(query, values);
     client.release();
+
+    if (result.rowCount === 0) {
+      return NextResponse.json(
+        { error: 'No se encontraron registros.' },
+        { status: 404 }
+      );
+    }
+
     return NextResponse.json(result.rows, { status: 200 });
   } catch (error) {
     console.error('Error al obtener history_salidas:', error);
